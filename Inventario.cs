c8ce79d41ebb6063fe6f91e830e6b7642093d4f3
Jostin654 <jostinvallecillo50@gmail.com>
2024-11-06 21:35:20 -0600

    Primer commit

---
+++ Inventario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeInventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+
+        public void AgregarProductos(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public IEnumerable<Producto> FiltrarYOrdenarProductos(decimal precioMinimo) 
+        {
+            return productos
+            .Where(p=> p.Precio>precioMinimo)
+            .OrderBy(p=> p.Precio);
+        }
+
+    }
+}
